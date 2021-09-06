db56f3c0463ba8653cca50a5d0bc3c477c120e8c
Dale B <daleban.dev@gmail.com>
2021-09-06 18:58:11 +0800

    Fix null data responsiveness.

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -15,24 +15,59 @@ const SearchPage = ({ drinks, showContent, loading, setShowContent }) => {
     Aos.init({});
   }, []);
 
+  //@emotion media query
+  const breakpoints = [576, 768, 992, 1200, 1320, 1480];
+
+  const mq = breakpoints.map((bp) => `@media (max-width: ${bp}px)`);
+
   return (
     <div
       className="SearchPage"
       css={css`
-        .nullData {
-          font-size: 3rem;
-          color: #fff;
-          font-family: "Poppins", sans-serif;
-          font-size: 10rem;
-          font-weight: 800;
-        }
+        .err-404 {
+          padding: 0 100px 50px 100px;
 
-        h2 {
-          font-size: 5rem;
-          font-family: "Nanum Brush Script", cursive;
-          color: #faa936;
-          position: relative;
-          left: 10rem;
+          ${mq[2]} {
+            padding: 40px;
+          }
+
+          .nullData {
+            font-size: 3rem;
+            color: #fff;
+            font-family: "Poppins", sans-serif;
+            font-size: 10rem;
+            font-weight: 800;
+
+            ${mq[2]} {
+              font-size: 8rem;
+            }
+
+            ${mq[1]} {
+              font-size: 6rem;
+            }
+
+            ${mq[0]} {
+              font-size: 4rem;
+            }
+          }
+
+          h2 {
+            font-size: 5rem;
+            font-family: "Nanum Brush Script", cursive;
+            color: #faa936;
+
+            ${mq[2]} {
+              font-size: 4rem;
+            }
+
+            ${mq[1]} {
+              font-size: 3rem;
+            }
+
+            ${mq[0]} {
+              font-size: 2rem;
+            }
+          }
         }
       `}
     >
@@ -43,10 +78,10 @@ const SearchPage = ({ drinks, showContent, loading, setShowContent }) => {
         </div>
       )}
       {drinks === null && !loading ? (
-        <>
+        <div className="err-404 container">
           <h2>uh ohh....</h2>
           <div className="nullData">no drinks matched</div>
-        </>
+        </div>
       ) : null}
       {showContent && drinks !== null && !loading ? (
         <div className="drink-container">
